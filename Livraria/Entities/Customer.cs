ba5c32b142a84207f71fbb98dbf96c300033b8d8
Vinícius Araújo Messias <vinicius.messias4321@gmail.com>
2025-10-07 22:48:21 -0300

    lil adjusts

--- Livraria/Entities/Customer.cs
+++ Livraria/Entities/Customer.cs
@@ -7,10 +7,13 @@ namespace Livraria.Entities
         public string Name { get; set; }
         public string Email { get; set; }
 
-        public void Update(string message)
+        public void Update(ISubject subject)
         {
-            Console.WriteLine(Name + ", " + message);
-            
+            if (subject is Book book)
+            {
+                Console.WriteLine($"Olá {Name}! O livro '{book.Title}' por {book.Author} está disponível por R${book.Price}.");
+            }
+
         }
     }
 }
